304f66fc362d190f2d58ed22d236552d5c478b63
dgaetan <dgaetan@ocm.com>
2023-01-21 14:58:42 -0300

    pokemon list

---
+++ mern/React/pokemonAPI/pokemon-list/src/App.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import './App.css';
+
+
+function App() {
+  const [poke,setPoke] = useState([]);
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+  
+    const fetchPokemon =()=>{
+      fetch(url)
+      .then((res)=>{
+        return res.json()
+      })
+      .then((res)=>{
+        console.log(res.results);
+        setPoke(res.results);
+      })
+      .catch((err)=> {
+        return err
+      })
+    }
+
+  return (
+    <div className="App">
+      <button className="btn btn-primary mt-2 mb-2" onClick={fetchPokemon}>Fetch em all!</button>
+      {
+        poke.map((pokemon,id)=> <p key={id}>{pokemon.name}</p>)
+      }
+    </div>
+  );
+}
+
+export default App;
